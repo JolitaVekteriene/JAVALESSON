2b855a9002aa4b1b5191caf0c091f14553545b50
JolitaVekteriene <100755789+JolitaVekteriene@users.noreply.github.com>
2022-05-05 14:53:12 +0300

    Box

---
+++ Serializer/src/main/java/ltcodeacademy/serializer/Box.java
@@ -0,0 +1,30 @@
+package ltcodeacademy.serializer;
+
+import java.io.Serializable;
+
+public class Box implements Serializable {
+    private String name;
+    private int size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Box(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+}
+
